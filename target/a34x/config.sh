1f3ebdda60bf2a3df3e026e5b501f65b971adae5
Quantum-A <emirefedikmen36@gmail.com>
2025-08-11 02:12:22 +0300

    target: a34x: hi!

---
+++ target/a34x/config.sh
@@ -0,0 +1,57 @@
+#
+# Copyright (C) 2024 Fede2782
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Device configuration file for Galaxy A34 5G (a34x)
+TARGET_NAME="Galaxy A34 5G"
+TARGET_CODENAME="a34x"
+TARGET_ASSERT_MODEL=("SM-A346E")
+TARGET_PLATFORM="mt6877v"
+TARGET_FIRMWARE="SM-A346E/TUR/355478282622413"
+TARGET_EXTRA_FIRMWARES=("")
+TARGET_API_LEVEL=35
+TARGET_PRODUCT_FIRST_API_LEVEL=33
+TARGET_VNDK_VERSION=31
+TARGET_SINGLE_SYSTEM_IMAGE="essi"
+TARGET_INSTALL_METHOD="odin"
+#TARGET_KEEP_ORIGINAL_SIGN=true
+TARGET_INCLUDE_PATCHED_VBMETA=true
+TARGET_OS_FILE_SYSTEM="erofs"
+TARGET_SUPER_PARTITION_SIZE=11744051200
+TARGET_SUPER_GROUP_SIZE=11739856896
+TARGET_HAS_SYSTEM_EXT=true
+TARGET_BOOT_DEVICE_PATH="/dev/block/by-name"
+TARGET_SUPER_GROUP_NAME="main"
+
+# SEC Product Feature
+TARGET_AUDIO_SUPPORT_ACH_RINGTONE=false
+TARGET_AUDIO_SUPPORT_DUAL_SPEAKER=true
+TARGET_AUDIO_SUPPORT_VIRTUAL_VIBRATION=false
+TARGET_AUTO_BRIGHTNESS_TYPE="5"
+TARGET_DVFS_CONFIG_NAME="dvfs_policy_mt6877_xx"
+TARGET_FP_SENSOR_CONFIG="google_touch_display_optical,settings=3"
+TARGET_MDNIE_SUPPORTED_MODES="37905"
+TARGET_MDNIE_WEAKNESS_SOLUTION_FUNCTION="0"
+TARGET_HAS_HW_MDNIE=false
+TARGET_MDNIE_SUPPORT_HDR_EFFECT=false
+TARGET_HAS_QHD_DISPLAY=false
+TARGET_HFR_MODE="1"
+TARGET_HFR_SUPPORTED_REFRESH_RATE="60,120"
+TARGET_HFR_DEFAULT_REFRESH_RATE="120"
+TARGET_HAS_MASS_CAMERA_APP=true
+TARGET_IS_ESIM_SUPPORTED=false
+TARGET_MULTI_MIC_MANAGER_VERSION="07010"
+TARGET_SSRM_CONFIG_NAME="siop_a34x_mt6877v"
