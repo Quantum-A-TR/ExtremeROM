1d7a9872322bb20ca8f4b66865874812de273d76
Quantum-A <emirefedikmen36@gmail.com>
2025-09-10 21:42:53 +0300

    un1ca: config: add mssi support (W.I.P)

---
+++ unica/configs/mssi.sh
@@ -0,0 +1,50 @@
+#
+# Copyright (C) 2025 Salvo Giangreco
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# ExtremeROM configuration file for Mediatek devices (mssi)
+
+# Galaxy Tab S10+ 5G (One UI 7)
+SOURCE_CODENAME="gts10p"
+SOURCE_FIRMWARE="SM-X826B/EUX/355200990202027"
+SOURCE_EXTRA_FIRMWARES=()
+SOURCE_API_LEVEL=35
+SOURCE_PRODUCT_FIRST_API_LEVEL=34
+SOURCE_VNDK_VERSION=34
+SOURCE_HAS_SYSTEM_EXT=true
+SOURCE_SUPER_GROUP_NAME="group_basic"
+
+# SEC Product Feature
+SOURCE_AUDIO_SUPPORT_ACH_RINGTONE=false
+SOURCE_AUDIO_SUPPORT_DUAL_SPEAKER=true
+SOURCE_AUDIO_SUPPORT_VIRTUAL_VIBRATION=false
+SOURCE_AUTO_BRIGHTNESS_TYPE="3"
+SOURCE_DVFS_CONFIG_NAME="dvfs_policy_default"
+SOURCE_FP_SENSOR_CONFIG="google_touch_display_optical,settings=3,screen_off,no_delay_in_screen_off"
+SOURCE_HAS_HW_MDNIE=true
+SOURCE_HAS_MASS_CAMERA_APP=false
+SOURCE_HAS_QHD_DISPLAY=false
+SOURCE_HFR_MODE="2"
+SOURCE_HFR_SUPPORTED_REFRESH_RATE="30,60,120"
+SOURCE_HFR_DEFAULT_REFRESH_RATE="120"
+SOURCE_HFR_SEAMLESS_BRT="102,102"
+SOURCE_HFR_SEAMLESS_LUX="250,3000"
+SOURCE_IS_ESIM_SUPPORTED=true
+SOURCE_MDNIE_SUPPORTED_MODES="65303"
+SOURCE_MDNIE_WEAKNESS_SOLUTION_FUNCTION="3"
+SOURCE_MULTI_MIC_MANAGER_VERSION="07010"
+SOURCE_SSRM_CONFIG_NAME="siop_gts10p_mt6989"
+SOURCE_SUPPORT_CUTOUT_PROTECTION=false
