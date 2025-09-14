270e8cae4b039d304320a448a2dd0bc1ef727972
Quantum-A <emirefedikmen36@gmail.com>
2025-09-14 22:16:14 +0300

    un1ca: patches: edit product_feature patch
    Removed TAProxy.smali line because Tab S10+ does not have it. Thanks to dupa z lasu!

--- unica/patches/product_feature/customize.sh
+++ unica/patches/product_feature/customize.sh
@@ -25,7 +25,6 @@ if [[ "$SOURCE_PRODUCT_FIRST_API_LEVEL" != "$TARGET_PRODUCT_FIRST_API_LEVEL" ]];
     system/framework/services.jar/smali/com/android/server/SystemServer.smali
     system/framework/services.jar/smali/com/android/server/enterprise/hdm/HdmVendorController.smali
     system/framework/services.jar/smali/com/android/server/enterprise/hdm/HdmSakManager.smali
-    system/framework/services.jar/smali/com/android/server/knox/dar/ddar/ta/TAProxy.smali
     system/framework/services.jar/smali_classes2/com/android/server/power/PowerManagerUtil.smali
     system/framework/services.jar/smali_classes2/com/android/server/sepunion/EngmodeService\$EngmodeTimeThread.smali
     "
